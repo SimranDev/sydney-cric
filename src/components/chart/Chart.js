69539dc0d1f1a1801fae1c371b869fd169f44807
SimranDev <simran.develop@gmail.com>
2021-05-19 18:39:12 +0530

    functions moved out of Chart component file

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -1,87 +1,60 @@
 import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import styled from "styled-components";
-import { indiaCount, getCountries, dynamoData } from "../../api/covid";
-import { options } from "./data";
+import { getTotalCases, getDateStamp, options } from "./data";
 import axios from "axios";
 
 const LineChart = () => {
-  const [dataState, setDataState] = useState([]);
-  const [labels, setLabels] = useState([]);
+  const [countriesData, setCountriesData] = useState([]);
 
   useEffect(() => {
     async function getData() {
       const dynamoData = await axios
-        .get(
-          "https://2zbwayfgle.execute-api.ap-southeast-2.amazonaws.com/prod/countries"
-        )
-        .then((response) => setDataState(response.data.countries))
+        .get(process.env.REACT_APP_GET_COUNTRIES_URL)
+        .then((response) => setCountriesData(response.data.countries))
         .catch((error) => console.log(error));
       return dynamoData;
     }
     getData();
   }, []);
 
-  const getDateStamp = () => {
-    const dates = dataState
-      .filter((item) => item.countryName === "USA")
-      .map((val) => new Date(val.dateStamp * 1000));
-    const dateSubString = dates.map((val) => val.toString().substring(4, 15));
-    return dateSubString;
-  };
-
   const data = {
-    labels: getDateStamp(),
+    labels: getDateStamp(countriesData),
     datasets: [
       {
         label: "USA",
-        data: dataState
-          .filter((item) => item.countryName === "USA")
-          .map((val) => val.total),
+        data: getTotalCases(countriesData, "USA"),
         fill: false,
         borderColor: "red",
         tension: 0.3,
-        yAxisID: "y",
       },
       {
         label: "India",
-        data: dataState
-          .filter((item) => item.countryName === "India")
-          .map((val) => val.total),
+        data: getTotalCases(countriesData, "India"),
         fill: false,
         borderColor: "orange",
         tension: 0.3,
-        yAxisID: "y",
       },
       {
         label: "Brazil",
-        data: dataState
-          .filter((item) => item.countryName === "Brazil")
-          .map((val) => val.total),
+        data: getTotalCases(countriesData, "Brazil"),
         fill: false,
         borderColor: "green",
         tension: 0.3,
-        yAxisID: "y",
       },
       {
         label: "France",
-        data: dataState
-          .filter((item) => item.countryName === "France")
-          .map((val) => val.total),
+        data: getTotalCases(countriesData, "France"),
         fill: false,
         borderColor: "blue",
         tension: 0.3,
-        yAxisID: "y",
       },
       {
         label: "Turkey",
-        data: dataState
-          .filter((item) => item.countryName === "Turkey")
-          .map((val) => val.total),
+        data: getTotalCases(countriesData, "Turkey"),
         fill: false,
         borderColor: "grey",
         tension: 0.3,
-        yAxisID: "y",
       },
     ],
   };
@@ -89,7 +62,6 @@ const LineChart = () => {
   return (
     <ParentContainer>
       <Line data={data} options={options} />
-      <button onClick={() => setLabels(getDateStamp)}>get dateStamp</button>
     </ParentContainer>
   );
 };
@@ -100,5 +72,4 @@ const ParentContainer = styled.div`
   margin-top: 300px;
   display: flex;
   width: 90%;
-  /* border: 1px dashed purple; */
 `;
--- src/components/chart/data.js
+++ src/components/chart/data.js
@@ -1,107 +1,18 @@
-// import { dynamoData } from "../../api/covid";
-
-// const indiaData = dynamoData.filter(
-//   (country) => country.countryName === "India"
-// );
-
-// const totalInd = indiaData.map((val) => val.total);
-
-const labels = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "June",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-// const data1 = totalInd;
-
-export const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Blue Rising Star",
-      data: [],
-      fill: false,
-      borderColor: "rgb(75, 192, 192)",
-      tension: 0.3,
-      yAxisID: "y",
-    },
-    // {
-    //   label: "Coogee Bay Rebels",
-    //   data: [25, 49, 88, 91, 66, 52, 66, 25, 32, 90, 61, 90],
-    //   fill: false,
-    //   borderColor: "purple",
-    //   tension: 0.3,
-    // },
-    // {
-    //   label: "Unpredictables",
-    //   data: [15, 49, 61, 33, 42, 88, 91, 32, 51, 55, 72, 81],
-    //   fill: false,
-    //   borderColor: "orange",
-    //   tension: 0.3,
-    // },
-    // {
-    //   label: "Spartan 11",
-    //   data: [12, 82, 76, 8, 61, 66, 44, 32, 88, 33, 61, 70],
-    //   fill: false,
-    //   borderColor: "blue",
-    //   tension: 0.3,
-    // },
-    // {
-    //   label: "Blacktown City Cricket",
-    //   data: [35, 62, 98, 55, 33, 73, 81, 61, 66, 21, 12, 26],
-    //   fill: false,
-    //   borderColor: "grey",
-    //   tension: 0.3,
-    // },
-  ],
+export const getDateStamp = (state) => {
+  const dates = state
+    .filter((item) => item.countryName === "USA")
+    .map((val) => new Date(val.dateStamp * 1000));
+  const dateSubString = dates.map((val) => val.toString().substring(4, 15));
+  return dateSubString;
 };
 
-//animation
-// const totalDuration = 10000;
-// const delayBetweenPoints = totalDuration / data1.length;
-// const previousY = (ctx) =>
-//   ctx.index === 0
-//     ? ctx.chart.scales.y.getPixelForValue(100)
-//     : ctx.chart
-//         .getDatasetMeta(ctx.datasetIndex)
-//         .data[ctx.index - 1].getProps(["y"], true).y;
-// const animation = {
-//   x: {
-//     type: "number",
-//     easing: "linear",
-//     duration: delayBetweenPoints,
-//     from: NaN, // the point is initially skipped
-//     delay(ctx) {
-//       if (ctx.type !== "data" || ctx.xStarted) {
-//         return 0;
-//       }
-//       ctx.xStarted = true;
-//       return ctx.index * delayBetweenPoints;
-//     },
-//   },
-//   y: {
-//     type: "number",
-//     easing: "linear",
-//     duration: delayBetweenPoints,
-//     from: previousY,
-//     delay(ctx) {
-//       if (ctx.type !== "data" || ctx.yStarted) {
-//         return 0;
-//       }
-//       ctx.yStarted = true;
-//       return ctx.index * delayBetweenPoints;
-//     },
-//   },
-// };
+export const getTotalCases = (state, country) => {
+  const totalCases = state
+    .filter((item) => item.countryName === country)
+    .map((val) => val.total);
+
+  return totalCases;
+};
 
 //options
 let delayed;
